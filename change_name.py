46e829f965e42e68080ec687a0b9338cef61ad4e
ndinhtuan <ndinhtuan15@gmail.com>
2024-12-24 16:53:17 +0100

    change name of disparity file

--- change_name.py
+++ change_name.py
@@ -4,8 +4,8 @@ import shutil
 
 if __name__=="__main__":
     
-    src_path_dir = "/media/tuan/Daten/project/dataset/cityscapes/cityscapes/disparity/train_wrongname"
-    dst_path_dir = "/media/tuan/Daten/project/dataset/cityscapes/cityscapes/disparity/train"
+    src_path_dir = "/media/tuan/Daten/project/projects/stereo_matching/RAFT_Stereo_pretrained/val_wrongname"
+    dst_path_dir = "/media/tuan/Daten/project/projects/stereo_matching/RAFT_Stereo_pretrained/val"
 
     for src_sub_path in glob.glob("{}/*".format(src_path_dir)):
 
@@ -15,6 +15,6 @@ if __name__=="__main__":
 
         for src_img_path in glob.glob("{}/*".format(src_sub_path)):
             img_name = src_img_path.split("/")[-1].split(".")[0]
-            new_img_name = "_".join(img_name.split("_")[:-1]) + "_disparity.png"
+            new_img_name = "_".join(img_name.split("_")[:-1]) + "_disparity.npy"
             dst_img_path = os.path.join(dst_sub_path, new_img_name)
             shutil.copy(src_img_path, dst_img_path)
